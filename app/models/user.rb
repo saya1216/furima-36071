0cf0214d7f2d6c88d313e050287a1d3dab84adbb
saya1216 <gmgmd087@gmail.com>
2021-09-11 17:11:03 +0900

    バリデーション・正規表現の追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,8 +6,8 @@ class User < ApplicationRecord
 
   validates :nickname, presence: true
   validates :password, format: { with: /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i }
-  validates :last_name, presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々]/ }
-  validates :first_name, presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々]/ }
+  validates :last_name, presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]/ }
+  validates :first_name, presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]/ }
   validates :last_name_kana, presence: true, format: { with: /\A[ァ-ヴー-]+\z/ }
   validates :first_name_kana, presence: true, format: { with: /\A[ァ-ヴー-]+\z/ }
   validates :birthday, presence: true
