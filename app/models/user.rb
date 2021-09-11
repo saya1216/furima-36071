9a205028ebeee2cb92bafb2a88143d4999d9ffdf
saya1216 <gmgmd087@gmail.com>
2021-09-11 16:02:47 +0900

    アソシエーションとルーティングの削除

--- app/models/user.rb
+++ app/models/user.rb
@@ -12,6 +12,4 @@ class User < ApplicationRecord
   validates :first_name_kana, presence: true, format: { with: /\A[ァ-ヴー-]+\z/ }
   validates :birthday, presence: true
 
-  has_many :items
-  has_many :buyers, dependent: :destroy
 end
