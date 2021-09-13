b2d16357b776a8ce44ed318f7cff8ae92803d14c
saya1216 <gmgmd087@gmail.com>
2021-09-13 21:38:05 +0900

    ActiveHashを導入

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,16 +1,22 @@
 class Item < ApplicationRecord
+  extend ActiveHash::Associations::ActiveRecordExtensions
 
   validates :image, presence: true
   validates :item_name, presence: true
   validates :information, presence: true
-  validates :category_id, presence: true
-  validates :sales_status_id, presence: true
-  validates :shipping_cost_id, presence: true
-  validates :prefecture_id, presence: true
-  validates :shipping_days_id, presence: true
+  validates :category_id, presence: true, numericality: { other_than: 1, message: "can't be blank" }
+  validates :sales_status_id, presence: true, numericality: { other_than: 1, message: "can't be blank" }
+  validates :shipping_cost_id, presence: true, numericality: { other_than: 1, message: "can't be blank" }
+  validates :prefecture_id, presence: true, numericality: { other_than: 1, message: "can't be blank" }
+  validates :shipping_days_id, presence: true, numericality: { other_than: 1, message: "can't be blank" }
   validates :price_id, presence: true
 
   belongs_to :user
+  belongs_to :category
+  belongs_to :prefecture
+  belongs_to :sales_status
+  belongs_to :shipping_cost
+  belongs_to :shipping_days
   has_one_attached :image
 
 end
