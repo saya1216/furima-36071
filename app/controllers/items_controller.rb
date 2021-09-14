a7a6f74505590dde41ed0b7491557b38ee2a9d39
saya1216 <gmgmd087@gmail.com>
2021-09-14 20:04:58 +0900

    Itemモデルのルーティング、newとcreateアクションを定義、出品ページの初期値を変更

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -4,6 +4,25 @@ class ItemsController < ApplicationController
   end
 
   def new
+    @item = Item.new
   end
+
+  def create
+    @item = Item.new(item_params)
+    if @item.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end  
+
+
+  private
+
+  def item_params
+    params.require(:item).permit(:item_name, :information, :image, :category_id, :sales_status_id, :shipping_cost_id, :prefecture_id, :shipping_days_id, :price_id).merge(user_id: current_user.id)
+  end
+
+
 
 end
