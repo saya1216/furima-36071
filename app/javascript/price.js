167b8d3d1f040f2f123d9f7905c215a93d072854
saya1216 <gmgmd087@gmail.com>
2021-09-20 18:29:37 +0900

    price.jsからデバッグに関する記述の削除

--- app/javascript/price.js
+++ app/javascript/price.js
@@ -2,16 +2,13 @@ function post (){
   const priceInput = document.getElementById("item-price");
   priceInput.addEventListener("input", () => {
     const inputValue = priceInput.value;
-      console.log(inputValue);
 
     const addTaxDom = document.getElementById("add-tax-price");
     addTaxDom.innerHTML = (Math.floor(inputValue * 0.1));
-      console.log(addTaxDom);
 
     const profitNumber = document.getElementById("profit");
     const valueResult = inputValue * 0.1;
     profitNumber.innerHTML = (Math.floor(inputValue - valueResult));
-      console.log(profitNumber);
   })
 };
 
