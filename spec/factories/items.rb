f0dde993ba64c3fd58429a4fa4818a20736f0359
saya1216 <gmgmd087@gmail.com>
2021-09-24 18:01:10 +0900

    カテゴリーの分類を修正

--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -2,7 +2,7 @@ FactoryBot.define do
   factory :item do
     item_name        { Faker::Lorem.characters(number: 40) }
     information      { Faker::Lorem.characters(number: 1_000) }
-    category_id      { Faker::Number.between(from: 2, to: 21) }
+    category_id      { Faker::Number.between(from: 2, to: 11) }
     sales_status_id  { Faker::Number.between(from: 2, to: 7) }
     shipping_cost_id { Faker::Number.between(from: 2, to: 3) }
     prefecture_id    { Faker::Number.between(from: 2, to: 48) }
