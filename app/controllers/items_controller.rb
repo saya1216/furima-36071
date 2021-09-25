d56f27e9e1c87931af33ac0038e881b52643e219
saya1216 <gmgmd087@gmail.com>
2021-09-25 11:58:32 +0900

    Itemコントローラーのリファクタリング

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,6 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show]
+  before_action :set_item, only:[:show, :edit, :update]
   before_action :move_to_index, only: :edit
 
   def index
@@ -20,17 +21,12 @@ class ItemsController < ApplicationController
   end
 
   def show
-    @item = Item.find(params[:id])
   end
 
   def edit
-    @item = Item.find(params[:id])
   end
 
   def update
-    
-    @item = Item.find(params[:id])
-  
     if @item.update(item_params)
       redirect_to item_path
     else
@@ -45,8 +41,11 @@ class ItemsController < ApplicationController
                                  :prefecture_id, :shipping_days_id, :price).merge(user_id: current_user.id)
   end
 
-  def move_to_index
+  def set_item
     @item = Item.find(params[:id])
+  end
+
+  def move_to_index
     unless current_user.id == @item.user_id
       redirect_to root_path
     end
