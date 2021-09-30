7d26193599f2b82789f2ba7c60d12d7c24b06920
saya1216 <gmgmd087@gmail.com>
2021-09-30 21:05:49 +0900

    Formオブジェクトを作成しバリデーションを記述、Userモデルのバリデーションをリファクタリング

--- app/models/buyer_address.rb
+++ app/models/buyer_address.rb
@@ -5,10 +5,10 @@ class BuyerAddress
   with_options presence: true do
     validates :user_id
     validates :item_id
-    validates :postal_code, format: {with: /\A[0-9]{3}-[0-9]{4}\z/, message: "is invalid. Include hyphen(-)"}
+    validates :postal_code, format: {with: /\A\d{3}-\d{4}\z/, message: "is invalid. Include hyphen(-)"}
     validates :city
     validates :house_number
-    validates :telephone
+    validates :telephone, numericality: {only_integer: true}, format: {with: /\A0\d{9,10}\z/}
     validates :buyer_id
   end
   validates :prefecture, numericality: {other_than: 1, message: "can't be blank"}
--- app/models/user.rb
+++ app/models/user.rb
@@ -4,13 +4,16 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  validates :nickname, presence: true
+  with_options presence: true do
+    validates :nickname
+    validates :last_name, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]/ }
+    validates :first_name, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]/ }
+    validates :last_name_kana, format: { with: /\A[ァ-ヴー-]+\z/ }
+    validates :first_name_kana, format: { with: /\A[ァ-ヴー-]+\z/ }
+    validates :birthday
+  end
+
   validates :password, format: { with: /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i }
-  validates :last_name, presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]/ }
-  validates :first_name, presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]/ }
-  validates :last_name_kana, presence: true, format: { with: /\A[ァ-ヴー-]+\z/ }
-  validates :first_name_kana, presence: true, format: { with: /\A[ァ-ヴー-]+\z/ }
-  validates :birthday, presence: true
 
   has_many :items
 end
