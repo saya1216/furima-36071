1fa2a8451840e13f65b9fc609a94043a3535d9f2
saya1216 <gmgmd087@gmail.com>
2021-10-06 20:36:11 +0900

    フォームのクレジット情報を削除

--- app/javascript/card.js
+++ app/javascript/card.js
@@ -19,9 +19,13 @@ const pay = () => {
         const token = response.id;
         const renderDom = document.getElementById("charge-form");
         const tokenObj = `<input value=${token} name='token' type="hidden">`;
-        renderDom.insertAdjacentHTML("beforeend", tokenObj);
-        
-      }
+        renderDom.insertAdjacentHTML("beforeend", tokenObj);        
+      };
+
+      document.getElementById("card_number").removeAttribute("name");
+      document.getElementById("card_exp_month").removeAttribute("name");
+      document.getElementById("card_exp_year") .removeAttribute("name");     
+      document.getElementById("card_cvc").removeAttribute("name");
     });
   });
 };
