247a2ab565377f2885cfb95419f39d47dfd55156
saya1216 <gmgmd087@gmail.com>
2021-11-07 06:28:43 +0900

    環境変数を読み込むための設定

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: "items#index"
+  resources :users, only: :new
   resources :items do
     resources :orders, only: [:index, :create]
   end
